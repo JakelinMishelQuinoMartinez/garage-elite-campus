47e0dfcecfa7a3a0fd2fe0415cda18b8102cb020
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-07 08:54:16 -0600

    feat(dml): insertar datos en la tabla de vehiculos

--- intermedio/ejercicio-clase-crud-procedimientos/resoluciones/jakelin-quino/dml/inserts.sql
+++ intermedio/ejercicio-clase-crud-procedimientos/resoluciones/jakelin-quino/dml/inserts.sql
@@ -7,3 +7,13 @@ INSERT INTO clientes (nombre, telefono, email) VALUES
 ('Lewis Hamilton', '44207946', 'lewis44@gmail.com'),
 ('Marc Márquez', '34611223', 'marc93@gmail.com'),
 ('Charles Lopéz', '77931528', 'charles16@gmail.com');
+
+-- Insertar Vehículos
+INSERT INTO vehiculos (cliente_id, tipo, marca, modelo, placa, anio) VALUES
+(1, 'auto_lujo', 'Ferrari', 'SF90 Stradale', 'P-345BTR', 2024),
+(2, 'moto', 'Yamaha', 'YZF-R1M', 'M-046MOT', 2023),
+(3, 'hiperdeportivo', 'Mercedes-AMG', 'ONE', 'P-444AMG', 2025),
+(4, 'moto', 'Ducati', 'Panigale V4 R', 'M-093MOT', 2024),
+(5, 'auto_lujo', 'Ferrari', 'Purosangue', 'P-516FRR', 2024),
+(1, 'hiperdeportivo', 'McLaren', 'Senna', 'P-777MCL', 2023),
+(2, 'moto', 'BMW', 'M 1000 RR', 'M-100RRR', 2024);
