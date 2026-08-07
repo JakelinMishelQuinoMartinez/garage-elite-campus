cf4983e82b19ebfa4ad941c293940a884a61cd6f
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-07 08:53:26 -0600

    feat(dml): insertar datos en la tabla de clientes

---
+++ intermedio/ejercicio-clase-crud-procedimientos/resoluciones/jakelin-quino/dml/inserts.sql
@@ -0,0 +1,9 @@
+USE garage_elite_campus;
+
+-- Insertar Clientes
+INSERT INTO clientes (nombre, telefono, email) VALUES
+('Carlos Sainz', '34600112', 'carlos.sainz@gmail.com'),
+('Valentino Rossi', '39066987', 'vale46@gmail.com'),
+('Lewis Hamilton', '44207946', 'lewis44@gmail.com'),
+('Marc Márquez', '34611223', 'marc93@gmail.com'),
+('Charles Lopéz', '77931528', 'charles16@gmail.com');
