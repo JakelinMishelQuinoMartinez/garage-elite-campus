1c3baa5216cfeb56d89051427320f41666b3da84
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-10 04:51:24 -0600

    feat(procedures): listar citas servcicos

--- intermedio/ejercicio-clase-crud-procedimientos/resoluciones/jakelin-quino/procedures/crud_citas.sql
+++ intermedio/ejercicio-clase-crud-procedimientos/resoluciones/jakelin-quino/procedures/crud_citas.sql
@@ -45,4 +45,27 @@ BEGIN
     SET p_cita_id = LAST_INSERT_ID();
 END //
 
+-- 2. CONSULTAR CITAS (Filtro Dinámico)
+CREATE PROCEDURE sp_listar_citas_servicio(
+    IN p_estado VARCHAR(20)
+)
+BEGIN
+    SELECT 
+        c.id AS cita_id,
+        cl.nombre AS cliente,
+        CONCAT(v.marca, ' ', v.modelo, ' (', v.placa, ')') AS vehiculo,
+        s.nombre AS servicio,
+        m.nombre AS mecanico,
+        c.fecha_programada,
+        c.estado,
+        c.precio_final
+    FROM citas_servicio c
+    JOIN vehiculos v ON c.vehiculo_id = v.id
+    JOIN clientes cl ON v.cliente_id = cl.id
+    JOIN servicios s ON c.servicio_id = s.id
+    JOIN mecanicos m ON c.mecanico_id = m.id
+    WHERE p_estado IS NULL OR c.estado = p_estado
+    ORDER BY c.fecha_programada ASC;
+END //
+
 DELIMITER ;
